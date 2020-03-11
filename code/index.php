b8cbc2281a94b169f650ffab22c4c0939be211f3
Andrey Pahomov <andrey.pahomov@gmail.com>
2020-03-12 01:16:39 +0300

    Add learn FANN code

--- code/index.php
+++ code/index.php
@@ -1,35 +1,35 @@
 <?php
 declare(strict_types=1);
 
+set_time_limit(0);
+
 require_once './vendor/autoload.php';
 
 use VPA\FANN\TrainBase as TrainBase;
 use VPA\FANN\TrainLabel as TrainLabel;
 use VPA\NeuronNetworks\Utils as Utils;
 
-$trainSet = new TrainBase('train-images-idx3-ubyte');
-$trainLabels = new TrainLabel('train-labels-idx1-ubyte');
+$ann = createNet();
 
-$items = $trainSet->getItems();
-$points = $trainSet[0];
+$parts = 10;
 
-$labels = $trainLabels->getItems();
-
-echo "<table border=1><tr>";
-for ($i=0;$i<20;$i++) {
-	$points = $trainSet[$i];
-	$value = reset($trainLabels[$i]);
-	printf('<td>%s</td><td>%d</td><td>%s</td>',show_image($points),$value,implode(",",Utils::toCategoretical($value,10)));
-	if (($i+1)%4==0) {
-		printf("</tr><tr>");
-	}
+$epochs = 5;
+for ($e = 0; $e<$epochs; $e++) {
+		$train_data = fann_read_train_from_file("train.data");
+		$mse_result = fann_train_epoch ($ann , $train_data );
+		printf("Epoch %d/%d MSE result: %.2f\n",$e,$epochs,$mse_result);
 }
-echo "</tr></table>";
 
+fann_save($ann,'minst.net');
 
-function createNet($trainSet,$trainLabels) {
-	$ann = fann_create_standard(4,784,800,400,10);
+var_dump($mse_result);
 
+function createNet() {
+	$ann = fann_create_standard( 4,784,800,400,10 );
+	fann_set_training_algorithm ( $ann , FANN_TRAIN_BATCH );
+	fann_set_activation_function_hidden( $ann, FANN_SIGMOID );
+	fann_set_activation_function_output( $ann, FANN_ERRORFUNC_TANH );
+	return $ann;
 }
 
 
